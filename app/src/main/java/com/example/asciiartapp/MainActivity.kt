62e46f14eaf0f96e5d8b1df04a094298c10241e1
trackofficial <tsikhenkoviktorr@gmail.com>
2026-01-30 14:53:09 +0300

    init: initial commit

---
+++ app/src/main/java/com/example/asciiartapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.asciiartapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+    }
+
+}
